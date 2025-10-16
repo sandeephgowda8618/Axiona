33cbd2c81135b2758127c2c3929b731940d137e9
Sandeep H <sandeephgowda8618@gmail.com>
2025-10-16 22:26:50 +0530

    Delete study_platform copy directory

--- study_platform copy/client/src/pages/PersonalGuideChat.tsx
+++
@@ -1,1 +0,0 @@
-// PersonalGuideChat page
--- study_platform copy/server/src/routes/index.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import roadmapRouter from './roadmap';
-
-const router = Router();
-router.use('/roadmap', roadmapRouter);
-
-export default router;
